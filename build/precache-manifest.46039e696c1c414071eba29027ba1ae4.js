e4da1094388007343c216b137ff96c6817e47178
touchy <prashantgecg@gmail.com>
2020-04-14 20:04:52 +0530

    push to servr with build

---
+++ build/precache-manifest.46039e696c1c414071eba29027ba1ae4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7170ef3e32b0999d31db2cf864e88ca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fed598cebfcaeaf8b720",
+    "url": "/static/css/2.f39ea103.chunk.css"
+  },
+  {
+    "revision": "33bf2518030df18f3c29",
+    "url": "/static/css/main.32da7a83.chunk.css"
+  },
+  {
+    "revision": "fed598cebfcaeaf8b720",
+    "url": "/static/js/2.1b44fdad.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.1b44fdad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33bf2518030df18f3c29",
+    "url": "/static/js/main.643a0056.chunk.js"
+  },
+  {
+    "revision": "111d1d5e311860f40786",
+    "url": "/static/js/runtime-main.d95113bc.js"
+  }
+]);
